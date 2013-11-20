84c13639c663aabc2177ff5343df73e51e1f128c
NXH <neil.hartz@gmail.com>
2013-11-20 16:41:31 -0600

    New index/show views for Events and Tasks

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,10 @@
 class Event < ActiveRecord::Base
   belongs_to :account
   belongs_to :category
+  scope :date_ascending, order('start_date asc')
+  scope :date_descending, order('start_date desc')
+
+  def time_format(time)
+    return time.to_formatted_s(:short)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :account
   belongs_to :category
+  scope :date_ascending, order('due_date asc')
+  scope :date_descending, order('due_date desc')
 end
