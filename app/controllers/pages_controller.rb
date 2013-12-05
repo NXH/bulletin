56f59d8641eec5e0b61be417404690fb5f9a09a7
NXH <neil.hartz@gmail.com>
2013-12-04 18:56:31 -0600

    Added customer date ranges to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,11 +9,34 @@ class PagesController < ApplicationController
   end
 
   def user_home
-    @tasks_person1 = current_account.tasks.where(:person1 => true)
-    @tasks_person2 = current_account.tasks.where(:person2 => true)
+    today = Date.today
+    tomorrow = Date.today.advance(days:1)
 
-    @events_person1 = current_account.events.where(:person1 => true)
-    @events_person2 = current_account.events.where(:person2 => true)
+    if params[:range] == "today"
+      @tasks_person1 = current_account.tasks.where(:person1 => true).where(:due_date => today).order("due_date")
+      @tasks_person2 = current_account.tasks.where(:person2 => true).where(:due_date => today).order("due_date")
+      @events_person1 = current_account.events.where(:person1 => true).where(:start_time => today.beginning_of_day..today.end_of_day).order("start_time")
+      @events_person2 = current_account.events.where(:person2 => true).where(:start_time => today.beginning_of_day..today.end_of_day).order("start_time")
+    elsif params[:range] == "tomorrow"
+      @tasks_person1 = current_account.tasks.where(:person1 => true).where(:due_date => tomorrow).order("due_date")
+      @tasks_person2 = current_account.tasks.where(:person2 => true).where(:due_date => tomorrow).order("due_date")
+      @events_person1 = current_account.events.where(:person1 => true).where(:start_time => tomorrow.beginning_of_day..tomorrow.end_of_day).order("start_time")
+      @events_person2 = current_account.events.where(:person2 => true).where(:start_time => tomorrow.beginning_of_day..tomorrow.end_of_day).order("start_time")
+    elsif params[:range]
+      advance = params[:range].to_i
+      date_range = Date.today..Date.today.advance(days:advance)
+      time_range = Date.today.beginning_of_day..Date.today.advance(days:advance).end_of_day
+      @tasks_person1 = current_account.tasks.where(:person1 => true).where(:due_date => date_range).order("due_date")
+      @tasks_person2 = current_account.tasks.where(:person2 => true).where(:due_date => date_range).order("due_date")
+      @events_person1 = current_account.events.where(:person1 => true).where(:start_time => time_range).order("start_time")
+      @events_person2 = current_account.events.where(:person2 => true).where(:start_time => time_range).order("start_time")
+    else
+      @tasks_person1 = current_account.tasks.where(:person1 => true).order("due_date")
+      @tasks_person2 = current_account.tasks.where(:person2 => true).order("due_date")
+
+      @events_person1 = current_account.events.where(:person1 => true).order("start_time")
+      @events_person2 = current_account.events.where(:person2 => true).order("start_time")
+    end
   end
 
 end
